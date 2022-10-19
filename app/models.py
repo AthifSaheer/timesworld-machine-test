c934d9676918fcccdf7f3231d8d995a118742657
Athif Saheer <liteboook@gmail.com>
2022-10-19 15:21:46 +0530

    Project initialized & model added

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+role_choice = [
+    ('student','student'),
+    ('staff','staff'),
+    ('admin','admin'),
+    ('editor','editor'),
+]
+
+class CustomUser(AbstractUser):
+    mobile_no = models.IntegerField(blank=True,null=True)
+    role = models.CharField(max_length=10, choices=role_choice)
+    country = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
